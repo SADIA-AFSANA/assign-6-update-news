e00b0aa5d1a8d98c1a75ff3221e9b8b72b5577c5
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-09-03 22:13:14 +0600

    container add

--- UpdateNews.js
+++ UpdateNews.js
@@ -18,6 +18,39 @@ const categoriesList = async () => {
     // console.log(category);
 }
 categoriesList()
+const displaynews = async (catId) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${catId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    newsDetails(data.data)
+}
+const newsDetails = newes => {
+    // console.log(newes);
+    const categoryContainer = document.getElementById('category-container');
+    newes.forEach(news => {
+        // console.log(news);
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+        <div class="card w-100 p-3">
+        <img src="${news.thumbnail_url}" class="card-img-top img-fluid" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details.slice(0, 260)}...</p>
+        
+        <img src="${news.author.img}" class="img-fluid w-25 rounded p-2">
+
+        <div class="d-flex justify-content-between px-2">
+        <p class="fw-bolder">${news.author.name}</p>
+        <p><span class="text-bold text-primary">views:</span>${news.total_view}M</p>
+        </div>
+        <button type="button" class="btn btn-primary">Details</button>
+        </div>
+        </div>
+        `;
+        categoryContainer.appendChild(newsDiv)
+    });
+}
 // const displaynews = categories => {
 //     const categoryContainer = document.getElementById('category-container');
 //     categories.forEach(category => {
